685973f9757581c2fd8faf0a294cb19663099f05
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-04-19 21:25:59 -0400

    added delete function to comments

--- backend/controllers/movies.js
+++ backend/controllers/movies.js
@@ -24,19 +24,49 @@ router.get('/:grab/movie', (req, res) => {
         .catch(console.error)
 })
 
+//CREATE
 router.put('/:grab/movie', (req, res) => {
     const id = req.params.grab;
     Movies.findOneAndUpdate({ _id: id}, {$push: {comments: req.body.comments}})
         .then( () => {
-            // res.render('./movies/movie', { movie: movie });
+            // res.redirect('./movies/movie')
             Movies.findById(id)
                 .then((movie) => {
                     // res.json(movie)
                     res.render('./movies/movie', { movie: movie })
+                    // res.redirect('./movies/id/movie')
                 })
         })
         .catch(console.error);
 })
+
+//DELETE
+router.put('/:grab/movie/:comment', (req, res) => {
+    const id = req.params.grab;
+    Movies.findOneAndUpdate({ _id: id}, {$pull: {comments: req.params.comment}})
+        .then( () => {
+            // res.redirect('./movies/movie')
+            Movies.findById(id)
+                .then((movie) => {
+                    // res.json(movie)
+                    res.render('./movies/movie', { movie: movie })
+                    // res.redirect('./movies/movie')
+                })
+        })
+        .catch(console.error);
+})
+
+
+// router.delete('/:grab/movie', (req, res) => {
+//     const id = req.params.grab;
+//     Movies.findByIdAndRemove(id)
+//         .then((movie) => {
+//             res.render('./movies/movie', { movie: movie })
+//         })
+//         .catch(console.error);
+// })
+
+
 
 
 
