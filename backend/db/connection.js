c1f536a53e3c1502699253e90e6cc65dc1934f6c
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-04-27 22:31:59 -0400

    check before pull

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,5 +1,20 @@
 
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+
+mongoose.connect('mongodb+srv://benjamin_m:Baseballbenji69@cluster0.9xnz1.mongodb.net/starwarsSocial?retryWrites=true&w=majority', { useNewUrlParser: true })
+.then((conn) => {
+    console.log(`connected to star wars social DB`);
+})
+.catch(err => {
+    console.error(err);
+})
+
+module.exports = mongoose;
+
+
 
 // const mongoose = require("mongoose");
 
@@ -15,23 +30,3 @@
 //     .catch(error => console.log(`failed conn:`, error))
 
 // module.exports = mongoose
-
-
-
-
-
-
-
-const mongoose = require('mongoose');
-
-mongoose.Promise = Promise;
-
-mongoose.connect('mongodb+srv://benjamin_m:Baseballbenji69@cluster0.9xnz1.mongodb.net/starwarsSocial?retryWrites=true&w=majority', { useNewUrlParser: true })
-.then((conn) => {
-    console.log(`connected to star wars social DB`);
-})
-.catch(err => {
-    console.error(err);
-})
-
-module.exports = mongoose;
