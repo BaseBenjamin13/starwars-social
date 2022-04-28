b9238b807dbb9a4a71cc723f326a2ab7a2390a8b
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-04-27 20:55:35 -0400

    added readMe no heroku link yet

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -6,6 +6,24 @@ const Movie = require('./models/movieM');
 const TvShows = require('./models/tv-showM');
 const Games = require('./models/gameM');
 
+
+
+
+// From Alana from Will Sutton
+// const importApiData = require(‘./rawg-game-seeds.json’);
+// importApiData.map(item => {
+//     return {
+//         id: item._id,
+//         //all the fields that i want
+        // rename fields to match api field and or 
+        // copy and paste your field and value type into here
+//         notes: ‘unowned’
+//     }
+// });
+
+
+
+
 Movie.deleteMany({})
     .then(() => {
         Movie.insertMany(movieSeed)
